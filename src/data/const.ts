760529648a95919f6b64b2ab34a09db33af0e1bb
Juan Intriago <juanintriagovillarreal@hotmail.com>
2022-03-19 17:15:08 -0500

    features

--- src/data/const.ts
+++ src/data/const.ts
@@ -1,3 +1,4 @@
 export const MAIN_IMAGE = require('./assets/main-background.png');
-export const ICON = require('./assets/abuelo.png');
+export const ICON = require('./assets/abuelo2.png');
 export const FAITHHOPE = require('./assets/faithhope.png');
+export const LOGIN_BACKGROUND = require('./assets/loginbackground.jpg');
